64121900e1dcad4a815d6acfa98886f6d5fdbc42
user <AhhVasALetiet13@gmail.com>
2026-06-21 14:31:44 +0300

    Door sound update

--- Assets/Systems/Rooms/RoomDoor.cs
+++ Assets/Systems/Rooms/RoomDoor.cs
@@ -22,11 +22,30 @@ public class RoomDoor : MonoBehaviour, IPlayerInteractableObject
     {
         IsOpen = open;
         meshRenderer.gameObject.SetActive(!open);
+
+        SoundManager.PlaySound(IsOpen ? openSound : closeSound, soundPlayer);
+        Creak();
     }
 
     public void Interact(Player player)
     {
         Open(!IsOpen);
     }
+
+    public void Creak()
+    {
+        if (Random.Range(0,100) <= 30)
+        {
+            SoundManager.PlaySound(creakSound, soundPlayer);
+        }
+    }
+
+    [Header("Sounds")]
+    public SoundPlayer soundPlayer;
+    public SoundData_RandomSound openSound;
+    public SoundData_RandomSound closeSound;
+    public SoundData_RandomSound monsterTryToOpenSound;
+    public SoundData_RandomSound creakSound;
+    public SoundData_RandomSound breakSound;
 
 }
