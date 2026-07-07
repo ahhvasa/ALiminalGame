38c4292151da01fcbd38db12f6e7ee894c8b0ff6
user <AhhVasALetiet13@gmail.com>
2026-07-08 01:34:39 +0300

    [WIP] RoomSystem integration

--- Assets/Systems/Objects/Rooms/RoomPartMark.cs
+++ Assets/Systems/Objects/Rooms/RoomPartMark.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class RoomPartMark : MonoBehaviour
 {
     public string addressiblesKey;
+    public Vector3 position;
 
     public List<RoomConnectedPart> roomConnectedParts = new();
 }
