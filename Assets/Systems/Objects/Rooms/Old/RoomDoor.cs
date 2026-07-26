24b27bec985142cac00e1f7b33856d21c91d6ba6
user <AhhVasALetiet13@gmail.com>
2026-07-26 07:40:20 +0300

    [WIP] Stable build

--- Assets/Systems/Objects/Rooms/Old/RoomDoor.cs
+++ Assets/Systems/Objects/Rooms/Old/RoomDoor.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Zenject.SpaceFighter;
-using static UnityEditor.Progress;
 
 public class RoomDoor : MonoBehaviour, IPlayerInteractableObject
 {
